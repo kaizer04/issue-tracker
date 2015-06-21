4ebaeaae71d0ec3d0fa8bc01daacd7d55516b7ce
kaizer04 <kaizer0505@gmail.com>
2015-06-21 22:43:19 +0300

    config views_dir in controllers constructs

--- controllers/issues.php
+++ controllers/issues.php
@@ -2,16 +2,24 @@
 
 namespace Controllers;
 
-class Issues_Controller {
+class Issues_Controller extends Master_Controller {
     public function __construct() {
-//        echo "Issue Controller<br />";
+        parent::__construct('/views/issues/');
     }
 
     public function index() {
 //        echo "Issues` index()<br/>";
 
-        $template_name = DX_ROOT_DIR . '/views/issues/index.php';
+        $template_name = DX_ROOT_DIR . $this->views_dir . 'index.php';
 
-        include_once DX_ROOT_DIR . '/views/layouts/default.php';
+        include_once $this->layout;
     }
+
+//    public function dve() {
+////        echo "Issues` index()<br/>";
+//
+//        $template_name = DX_ROOT_DIR . $this->views_dir . 'dve.php';
+//
+//        include_once $this->layout;
+//    }
 }
--- controllers/master.php
+++ controllers/master.php
@@ -4,8 +4,11 @@ namespace Controllers;
 
 class Master_Controller {
     protected $layout;
-    
-    public function __construct() {
+    protected $views_dir;
+
+    public function __construct($views_dir = 'views/master/') {
+        $this->views_dir = $views_dir;
+
         $this->layout = DX_ROOT_DIR . '/views/layouts/default.php';
 //        echo "Master Controller<br />";
     }
