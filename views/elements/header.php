3b446465ae6ec8e57b896971fe0c2116ea0efc4e
kaizer04 <kaizer0505@gmail.com>
2015-06-21 22:15:51 +0300

    add views/elements/footer.php add views/elements/header.php

---
+++ views/elements/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <style type="text/css">
+        body {
+            background-color: greenyellow;
+        }
+    </style>
+    <div id="container">
+        <div id="top-menu">
+            Top Menu
+        </div>
+        <?php
+            if(!empty($this->logged_user)) {
+                echo "<div id='userbar'>Hello, {$this->logged_user['username']}!</div>";
+            }
+        ?>
+        <div id="main">
+
