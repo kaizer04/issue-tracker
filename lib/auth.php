7e99f2f09e62a0f017b2e27c7538157619f6e357
kaizer04 <kaizer0505@gmail.com>
2015-06-24 00:50:50 +0300

    added login, logged functions

--- lib/auth.php
+++ lib/auth.php
@@ -7,8 +7,8 @@ class Auth {
     private static $logged_user = array();
 
     private function __construct() {
-        session_set_cookie_params(1800, "/");
-        session_start();
+//        session_set_cookie_params(1800, "/");
+//        session_start();
 
         if(!empty($_SESSION['username'])) {
             self::$is_logged_in = true;
@@ -36,23 +36,23 @@ class Auth {
         return self::$logged_user;
     }
 
-    public function login($username, $password){
-        $db_object = \Lib\Database::get_instance();
-        $db = $db_object->get_db();
-
-        $statement = $db->prepare(
-            "SELECT id, username FROM users WHERE username = ? AND password = MD5(?) LIMIT 1"
-        );
-        $statement->bind_param('ss', $username, $password);
-        $statement->execute();
-        $result_set = $statement->get_result();
-        if($row = $result_set->fetch_assoc()) {
-            $_SESSION['username'] = $row['username'];
-            $_SESSION['user_id'] = $row['id'];
-
-            return true;
-        }
-
-        return false;
-    }
+//    public function login($username, $password){
+//        $db_object = \Lib\Database::get_instance();
+//        $db = $db_object->get_db();
+//
+//        $statement = $db->prepare(
+//            "SELECT id, username FROM users WHERE username = ? AND password = MD5(?) LIMIT 1"
+//        );
+//        $statement->bind_param('ss', $username, $password);
+//        $statement->execute();
+//        $result_set = $statement->get_result();
+//        if($row = $result_set->fetch_assoc()) {
+//            $_SESSION['username'] = $row['username'];
+//            $_SESSION['user_id'] = $row['id'];
+//
+//            return true;
+//        }
+//
+//        return false;
+//    }
 }
