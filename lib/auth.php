3a5c4da26d58f34311411ecec921c786c4433ee0
kaizer04 <kaizer0505@gmail.com>
2015-06-24 00:53:50 +0300

    add session start

--- lib/auth.php
+++ lib/auth.php
@@ -7,8 +7,8 @@ class Auth {
     private static $logged_user = array();
 
     private function __construct() {
-//        session_set_cookie_params(1800, "/");
-//        session_start();
+        session_set_cookie_params(1800, "/");
+        session_start();
 
         if(!empty($_SESSION['username'])) {
             self::$is_logged_in = true;
